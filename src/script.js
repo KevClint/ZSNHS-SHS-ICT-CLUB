b2802bc7005af8564b51b798ed3536cbca184596
Clint Lorenzo <clintlorenzo11@gmail.com>
2025-08-24 15:20:41 +0800

    waza

--- src/script.js
+++ src/script.js
@@ -9,3 +9,13 @@
   }
   window.addEventListener("keydown", handleFirstTab);
 })();
+
+
+const mobileNav = document.querySelector(".hamburger");
+const menubar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  menubar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
